77d2c0c579cbe070681624f138b79f957a3471d8
Alex <sdqxxie@163.com>
2017-05-05 18:11:48 +0800

    增加蛙跳程序

--- watiao.py
+++ watiao.py
@@ -1,23 +1,24 @@
 # coding=utf-8
 
-# class Solution:
-#     def jumpFloor(self, number):
-#         if number == 1:
-#             return 1
-#         if number == 2:
-#             return 2
-#         else :
-#             return self.jumpFloor(number-1)+1 + self.jumpFloor(number-2)+2
-
 class Solution:
     def jumpFloor(self, number):
-        l=[]
-        l.append(0)
-        l.append(1)
-        l.append(2)
-        for i in range(3,number+1):
-            l.append(l[number-1] + l[number-2])
-        return l[number]
+        if number == 1:
+            return 1
+        if number == 2:
+            return 2
+        else :
+            return self.jumpFloor(number-1) + self.jumpFloor(number-2)
+
+# class Solution:
+#     def jumpFloor(self, number):
+#         l=[]
+#         l.append(0)
+#         l.append(1)
+#         l.append(2)
+#         for i in range(3,number+1):
+#             l.append( l[number-1] + l[number-2] )
+#         return l[number]
+
 s= Solution()
 n= s.jumpFloor(5)
 print '一共有%d种策略' % n
